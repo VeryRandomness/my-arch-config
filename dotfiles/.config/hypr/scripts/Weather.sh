3d582f71d75d113523c737104bf5e32a5e74ea0b
VeryRandomness <christopher.j.butters@gmail.com>
2025-10-03 11:10:40 -0600

    Update Weather.sh

--- dotfiles/.config/hypr/scripts/Weather.sh
+++ dotfiles/.config/hypr/scripts/Weather.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Configuration
 CACHE_FILE="/tmp/weather_cache"
-UPDATE_INTERVAL=60 # 1 minute in seconds
+UPDATE_INTERVAL=60
+# Tells which temporary file to use and how often to update (60 seconds)
 
+WTTR_URL="wttr.in?format=%l:+%t"
 # wttr.in format string: 
 # %l (location), %c (condition symbol), %t (temperature), %f (temperature feels like), %C (condition text), %w (wind), %h (humidity), %p (percipitation (mm/3 hours)), %P (pressure Hpa), %u (UV index), %m (moon phase symbol), %M (moon day), %s (sunest time (local time)), %S (sunrise time (local time))
 # you need a "+" sign for everything after the ":"
@@ -11,7 +12,6 @@ UPDATE_INTERVAL=60 # 1 minute in seconds
 # if you want the tempuratre to be in metric add "&%m" after "%t" NOTE: This will cause other things in your string to not show.
 # an example could be "https://wttr.in/?format=%l:+%t+%f+%w+%s+%S" this shows the locations (%l) the temp (%t) feels like (%f) wind (%w) and sunrise/sunset (%s/%S respectively)
 # I would recommend just using this that just tell you the location (Handy with a laptop) and the temperature. If you don't want the location. simply remove the "%l" and replace it with "Temperature" or leave it blank.
-WTTR_URL="wttr.in?format=%l:+%t"
 
 # Check if cache file exists
 if [[ -f "$CACHE_FILE" ]]; then
